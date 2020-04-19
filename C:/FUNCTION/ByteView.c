dd2f2d59c9711b3045f6385e9a25541dd197e975
B-World <59657499+B-World@users.noreply.github.com>
2020-04-19 15:26:48 +0800

    Add files via upload

---
+++ C:/FUNCTION/ByteView.c
@@ -0,0 +1,36 @@
+//#include"ByteView.h"
+#include<stdio.h>
+#include<Windows.h>
+#define MaxColumn 20//最大显示列数
+#define MaxLine 100//最大显示行数
+
+
+
+FILE* fp;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("#参数错误\n");
+		return -1;
+	}
+	fp = fopen(argv[1],"rb+");
+	if (!fp) {
+		printf("#文件不存在\n");
+		return -1;
+	}
+	fseek(fp, 0, SEEK_END);
+	int len = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	int counter = 0;
+	for (int a; (a = fgetc(fp)) != EOF; counter++) {
+		if (counter == MaxColumn) {
+			counter = 0;
+			printf("\n");
+		}
+		if (a < 17)printf("0");
+		printf("%x,", a);
+	}
+	printf("\n#文件长度:%d\n", len);
+	printf("#文件显示完毕");
+	
+}
