2e821c74523dd89c8ed74197587b40ed69186461
B-World <59657499+B-World@users.noreply.github.com>
2020-04-21 23:14:04 +0800

    Add files via upload

--- C:/FUNCTION/BPNetWork.c
+++ C:/FUNCTION/BPNetWork.c
@@ -5,63 +5,64 @@
 #include<string.h>
 
 //神经网络的层数
-#define LS network->layernum
-
-//神经网络隐藏层与输出层的层数
-#define AS network->layernum-1
+#define LS network->lns
 
 //输入层神经元的数量
-#define INNS network->neurons[0]
+#define INNS network->ns[0]
 
 //输入层的第a个输入
-#define INS(a) network->input[a-1]
+#define INS(a) network->is[a-1]
+
+//第a个理想输出
+#define TAS(a) network->ts[a-1]
 
 //输出层神经元的数量
-#define OUTNS network->neurons[network->layernum-1]
+#define OUTNS network->ns[LS-1]
 
 //第n层神经元的数量
-#define NS(n) network->neurons[n-1]
-
+#define NS(n) network->ns[n-1]
 
 //第n层第a个神经元的第p个权重
-#define WF(n,a,p) network->layers[n-2].ws[(p-1) + (a-1)*network->neurons[n-2]]
+#define WF(n,a,p) network->las[n-2].ws[(p-1)+(a-1)*NS(n-1)]
 
 //第n层的第a个神经元的偏置
-#define BF(n,a) network->layers[n-2].bs[a-1]
+#define BF(n,a) network->las[n-2].bs[a-1]
 
 //第n层第a个神经元的输出
-#define OF(n,a) network->layers[n-2].ps[a-1]
+#define OF(n,a) network->las[n-2].os[a-1]
 
 //第n层第a个神经元的误差
-#define SF(n,a) network->layers[n-2].ss[a-1]
+#define SF(n,a) network->las[n-2].ss[a-1]
+
+//学习率
+#define LN network->ln
 
 BPNetWork* BPCreate(int* nums, int len,double ln)
 {
 	BPNetWork* network = malloc(sizeof(BPNetWork));
-	network->layernum = len;
-	network->neurons = malloc(len * sizeof(int));
+	network->lns = len;
+	network->ns = malloc(len * sizeof(int));
 	network->ln = ln;
-	memcpy(network->neurons, nums, len * sizeof(int));
+	memcpy(network->ns, nums, len * sizeof(int));
 	//
-	network->input = malloc(nums[0] * sizeof(double));
-	network->layers = malloc(sizeof(Layer) * (len - 1));
-	network->out = malloc(sizeof(double) * nums[len - 1]);
+	network->is = malloc(nums[0] * sizeof(double));
+	network->las = malloc(sizeof(Layer) * (len - 1));
+	network->ts = malloc(sizeof(double) * nums[len - 1]);
 	for (int p = 0; p < len - 1; p++) {
-		srand(&__TIME__);
+		srand(&p);
 		int lastnum = nums[p];//上一层的神经元数量
 		int num = nums[p + 1];//当前层的神经元数量
-		network->layers[p].nums = num;
-		network->layers[p].bs = malloc(sizeof(double) * num);
+		network->las[p].bs = malloc(sizeof(double) * num);
 		//
-		network->layers[p].ws = malloc(sizeof(double) * num * lastnum);
+		network->las[p].ws = malloc(sizeof(double) * num * lastnum);
 		//
-		network->layers[p].ps = malloc(sizeof(double) * num);
+		network->las[p].os = malloc(sizeof(double) * num);
 		//
-		network->layers[p].ss = malloc(sizeof(double) * num);
+		network->las[p].ss = malloc(sizeof(double) * num);
 		for (int pp = 0; pp < num; pp++) {
-			network->layers[p].bs[pp] = rand() / RAND_MAX;
+			network->las[p].bs[pp] = 0.2;//rand() / 2.0 / RAND_MAX;
 			for (int ppp = 0; ppp < lastnum; ppp++) {
-				network->layers[p].ws[ppp + pp * lastnum] = rand() / RAND_MAX;
+				network->las[p].ws[ppp + pp * lastnum] = 0.5;//rand() / 2.0 / RAND_MAX;
 			}
 		}
 	}
@@ -72,30 +73,33 @@ void RunOnce(BPNetWork* network) {
 	//计算输入层到第二层
 	for (int a = 1; a <= NS(2); a++) {
 		double net = 0;
-		double* putout = &OF(2,a);//获取第2层的输出值
-		*putout = 0;
+		double* o = &OF(2,a);//获取第2层的输出值
+		*o = 0;
 		for (int aa = 1; aa <= INNS; aa++) {
-			net += INS(aa) * WF(2, a, aa);
+			net += INS(aa) * WF(2, a, aa);//INS(aa) * WF(2, a, aa);
 		}
-		*putout = f(net + BF(2,a));
+		*o = f(net + BF(2,a));
 	}
 	for (int n = 2; n <= LS-1; n++) {
 		for (int a = 1; a <= NS(n + 1); a++) {//下一层的神经网络
 			double net = 0;
-			double* putout = &OF(n+1,a);
-			*putout = 0;
+			double* o = &OF(n+1,a);
+			*o = 0;
 			for (int aa = 1; aa <= NS(n); aa++) {//当前层的神经网络
-				net += OF(n, aa) * WF(n + 1, a, aa);
+				double oo = OF(n, aa);
+				double* ww = &WF(n + 1, a, aa);
+				net += oo * (*ww);
 			}
-			*putout = f(net + BF(n + 1, a));
+			*o = f(net + BF(n + 1, a));
+			*o = *o;
 		}
 	}
 }
 
 //载入训练集
 void LoadIn(BPNetWork* network,double* input,double* putout) {
-	memcpy(network->input, input, INNS*sizeof(double));
-	memcpy(network->out, putout, OUTNS*sizeof(double));
+	memcpy(network->is, input, INNS*sizeof(double));
+	memcpy(network->ts, putout, OUTNS*sizeof(double));
 }
 
 //反向传播一次(训练一次)
@@ -106,14 +110,15 @@ void TrainOnce(BPNetWork* network) {
 		*s = 0;
 		double* b = &BF(LS, a);//获取第a个神经元的偏置
 		double o = OF(LS, a);//获取第a个神经元的输出
-		*s = 2 * (o - network->out[a]) * f_(o);
-		*b = *b - network->ln * (*s);//更新偏置
+		*s = (2.0 / OUTNS) * (o - TAS(a))* f_(o);
+		*b = *b - LN * (*s);//更新偏置
 		//更新权重
 		for (int aa = 1; aa <=NS(LS-1) ; aa++) {
 			double* w = &WF(LS, a, aa);
-			*w = *w - network->ln * (*s) * OF(LS - 1, aa);
+			*w = *w - LN * (*s) * OF(LS-1, aa);
 		}
 	}
+	
 	//计算隐藏层的误差
 	for (int a = LS-1; a > 2; a--) {
 		//开始计算第a层每个神经元的误差
@@ -127,14 +132,15 @@ void TrainOnce(BPNetWork* network) {
 				double ls = SF(a + 1, nn);//获取下一层第nn个神经元的误差
 				*s += ls * lw * f_(o);
 			}
-			*b = *b - network->ln * (*s);//更新偏置
+			*b = *b - LN * (*s);//更新偏置
 			//更新权重
 			for (int nn = 1; nn <= NS(a - 1); nn++) {//上一层
 				double* w = &WF(a, n, nn);
-				*w = *w - network->ln * (*s) *OF(a - 1, nn);
+				*w = *w - LN * (*s) *OF(a - 1, nn);
 			}
 		}
 	}
+	
 	//计算第2层的误差函数
 	for (int n = 1; n <= NS(2); n++) {//当前层
 		double* s = &SF(2, n);//获取第a个神经元的误差
@@ -146,60 +152,53 @@ void TrainOnce(BPNetWork* network) {
 			double ls = SF(3, nn);//获取下一层第nn个神经元的误差
 			*s += ls * lw * f_(o);
 		}
-		*b = *b - network->ln * (*s);//更新偏置
+		*b = *b - LN * (*s);//更新偏置
 		//更新权重
 		for (int nn = 1; nn <= INNS; nn++) {//上一层
 			double* w = &WF(2, n, nn);
-			*w = *w - network->ln * (*s) * INS(nn);
+			*w = *w - LN * (*s) * INS(nn);
 		}
 	}
+	
 }
 //输出总误差
 double ETotal(BPNetWork* network) {
 	double val = 0;
 	for (int a = 1; a <= OUTNS; a++) {
-		val += (OF(network->layernum, a) - network->out[a]) * (OF(network->layernum, a) - network->out[a]) / OUTNS;
+		val += ((OF(LS, a) - TAS(a)) * (OF(LS, a) - TAS(a))) / OUTNS;
 	}
 	return val;
 }
 
 int main() {
-	int a[] = { 5,100,100,5 };
-	double in[5] = { 10,10,10,10,10, };
-	double out[5] = { 0.1,0.1,0.1,0.1,0.1 };
+	int a[] = { 1,20,20,1 };
+	double in[1] = { 0.5 };
+	double in1[1] = { 0.2 };
 
 
-	double in1[5] = { 3,3,3,3,3, };
-	double out1[5] = { 0.5,0.1,0.3,0.1,0.2 };
-	BPNetWork* network = BPCreate(a, 4,0.5);
-	
-	
+	double out[1] = { 0.1 };
+
+
+
+	BPNetWork* network = BPCreate(a, 4, 0.5);
+
+	double* l = &BF(3, 1);
+
+	LoadIn(network, in, out);
 
 	while (1)
 	{
-		int w = 100;
-		LoadIn(network, in, out);
-		while (w--)
-		{
-			RunOnce(network);
-			TrainOnce(network);
-			printf("%g\n", ETotal(network));
-
-
-			//system("cls");
-		}
-		w = 100;
-		printf("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-		LoadIn(network, in1, out1);
-		while (w--)
-		{
-			RunOnce(network);
-			TrainOnce(network);
-			printf("%g\n", ETotal(network));
-		}
+		RunOnce(network);
+		TrainOnce(network);
+		double a = OF(4, 1);
+		printf("%g\n", a);
 	}
-	
-	
+
+
+
+
+
+
 	return 0;
 }
 
--- C:/FUNCTION/BPNetWork.h
+++ C:/FUNCTION/BPNetWork.h
@@ -5,18 +5,17 @@
 #define f_(x) Sigmoidf(x)//导函数
 
 typedef struct {
-	int nums;//神经元的数量
 	double* ws;//权重矩阵
 	double* bs;//偏置数组
-	double* ps;//输出数组
+	double* os;//输出数组
 	double* ss;//误差(总误差关于加权和的偏导)
 } Layer;
 typedef struct {
-	int layernum;//层数
-	int* neurons;//每层神经元的数量
-	double* input;//神经网络输入
-	double* out;//理想输出
-	Layer* layers;//神经网络各个层(不包括输入层)
+	int lns;//层数
+	int* ns;//每层神经元的数量
+	double* is;//神经网络输入
+	double* ts;//理想输出
+	Layer* las;//神经网络各个层(不包括输入层)
 	double ln;//学习率
 }BPNetWork;
 
@@ -37,11 +36,13 @@ void RunOnce(BPNetWork* network);
 
 
 //sigmoid激活函数
-#define Sigmoid(x) 1 / (1 + exp(-x))
+#define Sigmoid(x)  (1 / (1 + exp(-(x))))
 //sigmoid激活函数的导函数,输入为sigmoid输出
-#define Sigmoidf(f) f * (1 - f) 
-#define Tanh(x) (2 / (1 + exp(-2 * x)))-1
-#define Tanhf(f) 2*(1+f)*(1-f)
+#define Sigmoidf(f)  ((f) * (1 - (f)))
+
+
+#define Tanh(x) ((2 / (1 + exp(-2 * (x))))-1)
+#define Tanhf(f) ((1+(f))*(1-(f)))
 
 
 
