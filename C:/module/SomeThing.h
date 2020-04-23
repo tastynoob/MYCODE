537640046f4867dd3743728c7f0aa10fccc42c86
B-World <59657499+B-World@users.noreply.github.com>
2020-04-23 14:16:11 +0800

    Add files via upload

---
+++ C:/module/SomeThing.h
@@ -0,0 +1,31 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+//指定循环次数,fre:循环次数,par:指定(循环次数-1)的参数名
+#define cyc(fre,par)  \
+for (int par = 0; par < (fre); par++) 
+
+#define defs(name) \
+struct name 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
